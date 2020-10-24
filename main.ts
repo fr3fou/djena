7f4de993a47b056e1501f28549e2ac15738085af
Simo Aleksandrov <simo3003@me.com>
2020-10-25 00:42:11 +0300

    Tidy main again

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 import { word } from "./parse.ts"
 
 function main() {
-  // const helloParser = bind(char("h"), (ch) =>
-  //   bind(char("i"), (ch2) => result(ch + ch2))
-  // )
   console.log(word()("hii"))
 }
 main()
