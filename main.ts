5e64eeb0011f622def7a9a2a6512f09304837394
Simo Aleksandrov <simo3003@me.com>
2020-10-24 17:47:03 +0300

    "HEllo" parser!

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 // Parser takes in input and returns either an empty array or a singleton array.
 // Singleton array with a tuple of the parsed value and the rest of the input means it parsed successfully
 // Empty array means it failed parsing
-type Parser<T> = (s: string) => Array<[T, string]>;
-type char = string;
+type Parser<T> = (s: string) => Array<[T, string]>
+type char = string
 
 // result unconditionally returns the value
 function result<T>(v: T): Parser<T> {
-  return (input) => [[v, input]];
+  return (input) => [[v, input]]
 }
 
 // zero always fails
 function zero<T>(): Parser<T> {
-  return (_) => [];
+  return (_) => []
 }
 
 // item parses one character and returns the rest
@@ -19,11 +19,11 @@ function item(): Parser<char> {
   return (input) => {
     switch (input) {
       case "":
-        return [];
+        return []
       default:
-        return [[input[0], input.slice(1)]];
+        return [[input[0], input.slice(1)]]
     }
-  };
+  }
 }
 
 // bind combines 2 parsers
@@ -31,29 +31,47 @@ function bind<T, U>(p: Parser<T>, fn: (a: T) => Parser<U>): Parser<U> {
   return (input) =>
     p(input)
       .map(([output, inputPrime]) => fn(output)(inputPrime))
-      .flat();
+      .flat()
 }
 
 // sat parses characters, until the predicate returns false
 function sat(pred: (c: char) => boolean): Parser<char> {
-  return bind(item(), (ch) => (pred(ch) ? result(ch) : zero()));
+  return bind(item(), (ch) => (pred(ch) ? result(ch) : zero()))
 }
 
 // char parses a specific character
 function char(c: char): Parser<char> {
-  return sat((d) => d == c);
+  return sat((d) => d == c)
+}
+
+// digit parses a single digit
+function digit(): Parser<char> {
+  return sat((d) => d >= "0" && d <= "9")
+}
+
+// lowercase parses a lowercase character
+function lowercase(): Parser<char> {
+  return sat((d) => d.toLowerCase() == d)
+}
+
+// uppercase parses an uppercase character
+function uppercase(): Parser<char> {
+  return sat((d) => d.toUpperCase() == d)
 }
 
 function main() {
-  const bParser = char("b");
-  let start = "bbbbbbbbbbbbb";
-  while (true) {
-    const out = bParser(start);
-    if (out.length == 0) {
-      return;
-    }
-    console.log(out[0]);
-    start = out[0][1];
-  }
+  let examples = ["HEllo", "PEsho", "GOsho", "AAzzz"]
+
+  const helloParser = bind(char("H"), (ch1) =>
+    bind(char("E"), (ch2) =>
+      bind(char("l"), (ch3) =>
+        bind(char("l"), (ch4) =>
+          bind(char("o"), (ch5) => result([ch1, ch2, ch3, ch4, ch5]))
+        )
+      )
+    )
+  )
+  let start = "HEllo"
+  console.log(helloParser(start))
 }
-main();
+main()
