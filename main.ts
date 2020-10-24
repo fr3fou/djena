1e251a0a2e1a6cefeea30362929b8ef557f46f84
Simo Aleksandrov <simo3003@me.com>
2020-10-24 13:51:45 +0300

    Result parser

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 type Parser<T> = (s: string) => [[T, string]];
 
-// wip
-const parseInteger: Parser<number> = (s: string) => {
-  return [[5, s]];
-};
+function result<T>(v: T): Parser<T> {
+  return (input) => [[v, input]];
+}
+
+// // wip
+// const parseInteger: Parser<number> = (s: string) => {
+//   return [[5, s]];
+// };
